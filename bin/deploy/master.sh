999d02703fc3a005dca0f84e27cc82408bfc17ae
Eliezer Margareten <2771355@gmail.com>
2020-10-23 12:52:07 +0300

    updated deploy script

--- bin/deploy/master.sh
+++ bin/deploy/master.sh
@@ -7,38 +7,38 @@ fi
 
 echo -e "\e[1;32mDeploying application...\e[0m"
 
-echo "Entering maintenance mode."
+printf "Entering maintenance mode.\n"
 php artisan down
 
-echo "Updating codebase..."
+printf "Updating codebase...\n"
 git fetch origin master
 git reset --hard origin/master
 
-echo "Updating composer dependencies..."
+printf "Updating composer dependencies...\n"
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
-echo "Updating npm dependencies..."
+printf "Updating npm dependencies...\n"
 npm ci -q
 
-echo "Building assets..."
+printf "Building assets...\n"
 npm run prod -q
 
-echo "Optimizing code..."
+printf "Optimizing code...\n"
 php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# echo "Reloading PHP..."
+# printf "Reloading PHP...\n"
 # echo "" | sudo -S service php reload
 
-echo "Migrating database..."
+printf "Migrating database...\n"
 php artisan migrate --force
 
-# echo "Restarting queue workers..."
+# printf "Restarting queue workers...\n"
 # php artisan queue:restart
 
-echo "Exiting maintenance mode."
+printf "Exiting maintenance mode.\n"
 php artisan up
 
 echo -e "\e[1;32mApplication deployed! \e[0m"
