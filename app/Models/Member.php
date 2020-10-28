e3f0f64ec5b54d798ceb55e1712f620bf84c10d4
Eliezer Margareten <2771355@gmail.com>
2020-10-28 16:57:35 +0200

    added dev environment helpers

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @mixin IdeHelperMember
+ */
 class Member extends Model
 {
     use HasFactory, SoftDeletes, RouteBindingWithTrashed;
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @mixin IdeHelperUser
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
