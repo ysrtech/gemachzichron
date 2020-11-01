5d3bff37bc39abc6dee758d82ec50496c26a0262
Eliezer Margareten <2771355@gmail.com>
2020-11-01 13:31:15 +0200

    fir error page

--- resources/js/Pages/Error.vue
+++ resources/js/Pages/Error.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-      <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+      <div class="mx-auto sm:px-6 lg:px-8">
         <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-          <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider text-center">
+          <div class="text-lg text-gray-500 uppercase tracking-wider text-center">
             {{ message }}
           </div>
         </div>
