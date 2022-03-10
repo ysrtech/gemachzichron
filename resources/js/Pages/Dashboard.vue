f038af74ac18692e74870cd091dbb9e23690b205
chezky <sales@ysrtech.com>
2022-03-10 14:25:12 -0500

    Dashboard update

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,149 +1,113 @@
 <template>
   <main class="overflow-y-auto max-w-12xl mx-auto">
-    <div class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-<div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-        <div class="py-5 px-8 flex-1">
-          <div class="flex-col items-center">
-            <div class="shrink-0">
-              <h2 class="font-medium text-5xl">{{ loans_count }}</h2>
-            </div>
-            <div class="w-0 flex-1">
-              <span class="text-xl text-gray-500">Loans Given</span>
-            </div>
-          </div>
-        </div>
-        <inertia-link
-          :href="$route('loans.index')"
-          class="py-3 px-8 bg-gray-50 hover:bg-gray-100 text-gray-700 hover:text-gray-900 text-sm font-medium hover:underline">
-          View Loans
-        </inertia-link>
+    <div class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow p-5">
+        <div class="text-xl text-gray-500">Loans</div>
+        <h2 class="flex-1 font-medium text-4xl">{{ loans_count }}</h2>
       </div>
-      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-        <div class="py-5 px-8 flex-1">
-          <div class="flex-col items-center">
-            <div class="shrink-0">
-              <h2 class="font-medium text-5xl"><money class="text-4xl" :amount="total_loans"/></h2>
-            </div>
-            <div class="w-0 flex-1">
-              <span class="text-xl text-gray-500">
-                Total In Loans Given
-              </span>
-            </div>
-          </div>
-        </div>
-        <inertia-link
-          :href="$route('loans.index')"
-          class="py-3 px-8 bg-gray-50 hover:bg-gray-100 text-gray-700 hover:text-gray-900 text-sm font-medium hover:underline">
-          View Loans
-        </inertia-link>
-      </div>
-      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-          <div class="shrink-0 flex-1">
-              <h2 class="font-medium text-5xl"><money class="text-4xl" :amount="total_loans_outstanding"/></h2>
-            </div>
-          <div class="bg-gray-50 py-3 px-8">
-            <span class="text-gray-700 font-medium text-sm">
-              Total Loans Outstanding
-            </span>
-          </div>
-        </div>
 
-         <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-        <div class="py-5 px-8 flex-1">
-          <div class="flex-col items-center">
-            <div class="shrink-0">
-              <h2 class="font-medium text-5xl"><money class="text-4xl" :amount="total_capital"/></h2>
-            </div>
-            <div class="w-0 flex-1">
-              <span class="text-xl text-gray-500">
-                Total Capital
-              </span>
-            </div>
-          </div>
-        </div>
-        <inertia-link
-          :href="$route('loans.index')"
-          class="py-3 px-8 bg-gray-50 hover:bg-gray-100 text-gray-700 hover:text-gray-900 text-sm font-medium hover:underline">
-          View Loans
-        </inertia-link>
+      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow p-5">
+        <div class="text-xl text-gray-500">Loans Total</div>
+        <h2 class="font-medium text-5xl flex-1">
+          <money class="text-4xl" :amount="total_loans" />
+        </h2>
       </div>
-       <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-        <div class="py-5 px-8 flex-1">
-          <div class="flex-col items-center">
-            <div class="shrink-0">
-              <h2 class="font-medium text-5xl"><money class="text-4xl" :amount="total_membership_fees"/></h2>
-            </div>
-            <div class="w-0 flex-1">
-              <span class="text-xl text-gray-500">
-                Total Membership & Decline Fees
-              </span>
-            </div>
-          </div>
-        </div>
-        <inertia-link
-          :href="$route('loans.index')"
-          class="py-3 px-8 bg-gray-50 hover:bg-gray-100 text-gray-700 hover:text-gray-900 text-sm font-medium hover:underline">
-          View Loans
-        </inertia-link>
+
+      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow p-5">
+        <div class="text-xl text-gray-500">Outstanding Loans</div>
+        <h2 class="font-medium text-5xl flex-1">
+          <money class="text-4xl" :amount="total_loans_outstanding" />
+        </h2>
       </div>
-      <!-- stats -->
+
+      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow p-5">
+        <div class="text-xl text-gray-500">Gemach Capital</div>
+        <h2 class="font-medium text-4xl">
+          <money class="text-4xl" :amount="total_capital" />
+        </h2>
+      </div>
+
+      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow p-5">
+        <div class="text-xl text-gray-500">Membership & Decline Fees</div>
+        <h2 class="font-medium text-4xl">
+          <money class="text-4xl" :amount="total_membership_fees" />
+        </h2>
+      </div>
+
       <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-        <div class="py-5 px-8 flex-1">
-          <div class="flex items-center">
-            <div class="shrink-0">
-              <h2 class="font-medium text-5xl">{{ pending_manual_count }}</h2>
-            </div>
-            <div class="ml-5 w-0 flex-1">
-              <span class="text-xl text-gray-500">
-                Pending Manual Transactions
-              </span>
-            </div>
-          </div>
+        <div class="flex flex-col p-5">
+          <div class="text-xl text-gray-500">Pending Manual Transactions</div>
+          <h2 class="font-medium text-4xl">{{ pending_manual_count }}</h2>
         </div>
         <inertia-link
-          :href="$route('transactions.index', {
-            status: TRANSACTION_STATUSES.Pending, gateway: AVAILABLE_GATEWAYS.Manual
-          })"
-          class="py-3 px-8 bg-gray-50 hover:bg-gray-100 text-gray-700 hover:text-gray-900 text-sm font-medium hover:underline">
+          :href="
+            $route('transactions.index', {
+              status: TRANSACTION_STATUSES.Pending,
+              gateway: AVAILABLE_GATEWAYS.Manual,
+            })
+          "
+          class="
+            py-3
+            px-8
+            bg-gray-50
+            hover:bg-gray-100
+            text-gray-700
+            hover:text-gray-900
+            text-sm
+            font-medium
+            hover:underline
+          "
+        >
           View
         </inertia-link>
       </div>
       <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-        <div class="py-5 px-8 flex-1">
-          <div class="flex items-center">
-            <div class="shrink-0">
-              <h2 class="font-medium text-5xl">{{ failed_count }}</h2>
-            </div>
-            <div class="ml-5 w-0 flex-1">
-              <span class="text-xl text-gray-500">
-                Failed Transactions
-              </span>
-            </div>
-          </div>
+        <div class="flex flex-col p-5">
+          <div class="text-xl text-gray-500">Failed Transactions</div>
+          <h2 class="font-medium text-4xl">{{ failed_count }}</h2>
         </div>
         <inertia-link
-          :href="$route('transactions.index', {status: TRANSACTION_STATUSES.Fail})"
-          class="py-3 px-8 bg-gray-50 hover:bg-gray-100 text-gray-700 hover:text-gray-900 text-sm font-medium hover:underline">
+          :href="
+            $route('transactions.index', { status: TRANSACTION_STATUSES.Fail })
+          "
+          class="
+            py-3
+            px-8
+            bg-gray-50
+            hover:bg-gray-100
+            text-gray-700
+            hover:text-gray-900
+            text-sm
+            font-medium
+            hover:underline
+          "
+        >
           View
         </inertia-link>
       </div>
-      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow">
-          <div class="py-5 px-8 flex-1">
-            <money class="text-4xl" :amount="month_success_total"/>
-          </div>
-          <div class="bg-gray-50 py-3 px-8">
-            <span class="text-gray-700 font-medium text-sm">
-              Successful Transactions Total &dash; {{ currentDate({year: 'numeric', month: 'long'}) }}
-            </span>
-          </div>
+      <div class="bg-white flex flex-col overflow-hidden rounded-lg shadow p-5">
+        <div class="text-xl text-gray-500">
+          Successful Transactions &dash;{{
+            currentDate({ year: "numeric", month: "long" })
+          }}
         </div>
+        <h2 class="font-medium text-4xl">
+          <money :amount="month_success_total" />
+        </h2>
+      </div>
 
-
-      <app-panel class="col-span-1 sm:col-span-2 lg:col-span-3 mt-8 sm:overflow-auto" title="Recent Transactions">
+      <app-panel
+        class="col-span-1 sm:col-span-2 lg:col-span-4 mt-8 sm:overflow-auto"
+        title="Recent Transactions"
+      >
         <template #content>
-          <transactions-table :transactions="recent_transactions"/>
+          <transactions-table :transactions="recent_transactions" />
           <div class="text-gray-700 text-center font-medium py-3">
-            <inertia-link class="hover:underline" :href="$route('transactions.index')">View All</inertia-link>
+            <inertia-link
+              class="hover:underline"
+              :href="$route('transactions.index')"
+              >View All</inertia-link
+            >
           </div>
         </template>
       </app-panel>
@@ -152,21 +116,21 @@
 </template>
 
 <script>
-import AppLayout from '@/Layouts/AppLayout'
-import {TRANSACTION_STATUSES} from '@/config/transactions';
-import {AVAILABLE_GATEWAYS} from "@/config/gateways";
-import {currentDate} from "@/helpers/dates";
+import AppLayout from "@/Layouts/AppLayout";
+import { TRANSACTION_STATUSES } from "@/config/transactions";
+import { AVAILABLE_GATEWAYS } from "@/config/gateways";
+import { currentDate } from "@/helpers/dates";
 import Money from "@/Components/Money";
 import TransactionsTable from "@/Components/App/Transactions/TransactionsTable";
 import AppPanel from "@/Components/Panel";
 
 export default {
-  layout: (h, page) => h(AppLayout, {title: 'Dashboard'}, () => page),
+  layout: (h, page) => h(AppLayout, { title: "Dashboard" }, () => page),
 
   components: {
     AppPanel,
     TransactionsTable,
-    Money
+    Money,
   },
 
   props: {
@@ -178,19 +142,18 @@ export default {
     total_loans: [Number, String],
     total_loans_outstanding: [Number, String],
     total_membership_fees: [Number, String],
-    total_capital: [Number, String]
-
+    total_capital: [Number, String],
   },
 
   data() {
     return {
       TRANSACTION_STATUSES,
       AVAILABLE_GATEWAYS,
-    }
+    };
   },
 
   methods: {
-    currentDate
-  }
-}
+    currentDate,
+  },
+};
 </script>
